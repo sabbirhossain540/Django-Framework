22356fc3a6eab72037793303c311970db911ee2c
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-01-07 08:37:25 +0600

    21 Days Challenge (Day 7)

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-print("Introduction To Django Framework")
+// Create Environment
+conda create --name myDjango django
+
+//View Conda Info
+conda info --envs
+
+//Environment Activation
+$ conda activate myDjango
+
+//Environment Deactivation
+$ conda deactivate
+
+//Create a Django Project
+django-admin startproject first_project
+
+//Run a Django Project
+python manage.py runserver
+
+//Create a Django application
+python manage.py startapp first_app
