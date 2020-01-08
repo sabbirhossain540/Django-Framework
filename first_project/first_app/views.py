9e900fa1d5cc54fa75ad923110cee34ae955f694
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-01-08 08:40:46 +0600

    21 Days Challenge (Day 8)

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    my_dict = {'insert_me':'Now I am Comming From first.index.html'}
+    return render(request,'first_app/index.html',context=my_dict)
+    #return HttpResponse("Hello World")
--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+#from django.urls import path
+from django.conf.urls import url
+from django.conf.urls import include
+from first_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$',views.index,name = 'index'),
+    url(r'^mynewextension/',include('first_app.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
