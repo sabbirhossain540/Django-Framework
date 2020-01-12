8afcd50f4dd2bd1e915fc64172b6683b7548d0fd
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-01-12 08:47:12 +0600

    21 Days Challenge (Day 12)

--- first_project/first_app/forms.py
+++ first_project/first_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.core import validators
 
+#Customized Validation for Input Box
 def check_for_z(value):
     if value[0].lower() != 'z':
         raise forms.ValidationError("Need To Start With Z")
@@ -8,10 +9,23 @@ def check_for_z(value):
 class FormName(forms.Form):
     name = forms.CharField(validators=[check_for_z])
     email = forms.EmailField()
+    varifyEmail = forms.EmailField(label='Enter Your Email Again:')
     text = forms.CharField(widget=forms.Textarea)
+
+    #Hidden Input Validation
     botcatcher = forms.CharField(required=False,
                                 widget=forms.HiddenInput,
                                 validators=[validators.MaxLengthValidator(0)])
+
+    #Email Match Validation
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vemail = all_clean_data['varifyEmail']
+
+        if email != vemail:
+            raise forms.ValidationError("Make Sure Email Match!")
+
 
     #Customized Validator
     # def clean_botcatcher(self):
